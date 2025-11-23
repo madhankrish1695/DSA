3703ac3861559d8f3d1ffca1ad63cec0934dd132
madhankrish1695 <Madhan@16295>
2025-11-23 21:34:36 +0530

    Leetcode - IntegerToRoman

---
+++ LeetCode/src/main/java/com/leetcode/arrays/IntegerToRoman.java
@@ -0,0 +1,26 @@
+package com.leetcode.arrays;
+
+public class IntegerToRoman {
+
+    public static void main(String[] args) {
+        IntegerToRoman integerToRoman = new IntegerToRoman();
+        System.out.println(integerToRoman.intToRoman(3999));
+        System.out.println(integerToRoman.intToRoman(3749));
+    }
+
+    public String intToRoman(int num) {
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return sb.toString();
+    }
+}
