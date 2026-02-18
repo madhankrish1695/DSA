409b12ea784ebb78c9c00edbc9a12cf4fca8a1e2
Madhana Gopal Radhakrishnan <rmgopal95@gmail.com>
2026-02-18 08:43:51 +0530

    Leetcode - BinaryTree - BSTIterator

---
+++ LeetCode/src/main/java/com/leetcode/binaryTree/BSTIterator.java
@@ -0,0 +1,57 @@
+package com.leetcode.binaryTree;
+
+import java.util.Stack;
+
+/**
+ * Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+ * <p>
+ * BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+ * boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+ * int next() Moves the pointer to the right, then returns the number at the pointer.
+ * Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+ * <p>
+ * You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+ */
+public class BSTIterator {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(7);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(9);
+        root.right.right = new TreeNode(20);
+
+        BSTIterator iterator = new BSTIterator(root);
+
+        System.out.println("BST Inorder Traversal using Iterator:");
+
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        pushLeft(root);
+    }
+
+    private void pushLeft(TreeNode root) {
+        if (root != null) {
+            stack.push(root);
+            pushLeft(root.left);
+        }
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+        if (node.right != null) {
+            pushLeft(node.right);
+        }
+        return node.val;
+    }
+}
